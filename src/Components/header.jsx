6541b27bef8e4245801510fd8690eb358b0763bd
omkar273 <omkarsonawane622@gmail.com>
2023-03-04 15:03:29 +0530

    fixed links

--- src/Components/header.jsx
+++ src/Components/header.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import '../StyleSheets/header.css'
 const Header = () => {
     return (
         <div >
             <header class="nav_container">
                 <nav class="nav">
-                    <a class="active" href="/">Home</a>
-                    <a href="/inc">search</a>
-                    <a href="/data">Explore</a>
-                    <a href="/netflix">Movies</a>
+                    <Link class="active" to={'/'}>Home</Link>
+                    <Link to={"/inc"}>search</Link>
+                    <Link to={"/data"}>Explore</Link>
+                    <Link to={"/netflix"}>Movies</Link>
                     <a href="#news">Web-series</a>
                 </nav>
             </header>
